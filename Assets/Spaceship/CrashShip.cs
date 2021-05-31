66edd9de2599a4d906294383c82e972f369b633b
adamglueck <adam.glueck1@gmail.com>
2021-05-30 23:25:53 -0400

    Added Spaceships and switching

---
+++ Assets/Spaceship/CrashShip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashShip : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+       // GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+        //GetComponent<Rigidbody>().AddTorque(20, 1, 1);
+        //create a convenient reference to transform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+
+    }
+}
